ef09174e12196984d2f7a5d7dfa22d9ea1e8cee2
frank-Mwangi <kungofrank@gmail.com>
2023-08-31 20:50:43 +0300

    Validate provided password against hashed record

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,7 @@
 """
 Password encrypting module
 """
+from xmlrpc.client import Boolean
 import bcrypt
 
 
@@ -9,3 +10,8 @@ def hash_password(password: str) -> bytes:
     """apply bcrypt encoding on plaintext password"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> Boolean:
+    """Validate provided password against hashed record"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
