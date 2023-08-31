1e3d476f13258c33cf07413a206d739ffa0bf42b
frank-Mwangi <kungofrank@gmail.com>
2023-08-31 20:43:32 +0300

    Encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!//bin/env python3
+"""
+Password encrypting module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """apply bcrypt encoding on plaintext password"""
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
