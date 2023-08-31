bc03f552b69091095ec193f4f02ca7133f66f44a
frank-Mwangi <kungofrank@gmail.com>
2023-08-31 18:58:43 +0300

    Filter values in incoming log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ def filter_datum(fields: List[str],
                  separator: str) -> str:
     """Return log message obfuscated"""
     for field in fields:
-        message = re.sub(f'{field}=(.?){separator}',
+        message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
 
