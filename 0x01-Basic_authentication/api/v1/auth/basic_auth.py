7951ab18a6e578918cd9bf10e07e7bc1464196b2
frank-Mwangi <kungofrank@gmail.com>
2023-09-07 07:39:36 +0300

    Encode auth headers in Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Defining the BasicAuth class"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """Encode auth header"""
         if authorization_header is None:
             return None
