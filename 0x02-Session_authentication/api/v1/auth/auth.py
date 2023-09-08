0220687f526da3eeeb1c8722fb3f4a40204b4d1f
frank-Mwangi <kungofrank@gmail.com>
2023-09-08 11:46:37 +0300

    Create session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ API authentication class
 
 from typing import List, TypeVar
 from flask import request
+import os
 
 
 class Auth:
@@ -35,3 +36,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """handles the Flask request object"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = os.getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
