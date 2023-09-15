e0e7cf7bafbab2b90f04dcdc0af9d341f3fd6c45
frank-Mwangi <kungofrank@gmail.com>
2023-09-15 10:42:32 +0300

    Add destroy session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Update user's session ID to None"""
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
